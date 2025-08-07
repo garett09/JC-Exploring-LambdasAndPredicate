9ab618c25948f1597076ddf33d46cf2bf21ac4fb
Adrian Garett Sian <adriansian@gmail.com>
2025-08-07 16:07:56 +0800

    Main: Demonstrates Java Predicate functional interface
    Replaces the initial boilerplate application with a comprehensive example of the `java.util.function.Predicate`.
    
    This change showcases how to define, apply, and combine predicates for filtering collections. It includes demonstrations of chaining predicates with `and()` and `negate()` methods to build complex logical conditions.
    
    Signed-off-by: Adrian Garett Sian

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,56 @@
 package org.example;
+import java.util.function.Predicate;
+import java.util.List;
+import java.util.ArrayList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void filterAndPrint(List<String> list, Predicate<String> predicate, String description) {
+        System.out.println("--- " + description + " ---");
+        for (String item : list) {
+            if (predicate.test(item)) {
+                System.out.println(item);
+            }
         }
+        System.out.println(); // Add a blank line for readability
+    }
+    public static void main(String[] args) {
+
+        Predicate<String> isLong = s -> s.length() > 10;
+
+        String str1 = "short";
+        String str2 = "This is a very long string";
+
+        System.out.println("Is '" + str1 + "' long? " + isLong.test(str1));
+        System.out.println("Is '" + str2 + "' long? " + isLong.test(str2));
+
+        System.out.println();
+
+        List<String> callSigns = new ArrayList<>();
+        callSigns.add("Alpha");
+        callSigns.add("Bravo");
+        callSigns.add("Archangel");
+        callSigns.add("Echo");
+        callSigns.add("Avenger");
+
+        // Define a predicate with a lambda to find call signs starting with "A"
+        Predicate<String> startsWithA = s -> s.startsWith("A");
+        Predicate<String> hasLengthGreaterThan5 = s -> s.length() > 5;
+
+        // Pass the list and the predicate (the behavior) to our method
+        filterAndPrint(callSigns, startsWithA, "Call signs starting with 'A'");
+
+
+        // Let's find call signs that start with 'A' AND have a length > 5
+        Predicate<String> complexCondition = startsWithA.and(hasLengthGreaterThan5);
+        filterAndPrint(callSigns, complexCondition, "Starts with 'A' AND length > 5");
+
+        // Now let's find call signs that do NOT start with 'A'
+        Predicate<String> doesNotStartWithA = startsWithA.negate();
+        filterAndPrint(callSigns, doesNotStartWithA, "Does NOT start with 'A'");
+
+
     }
 }
